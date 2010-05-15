144f3ecdf04c411b7fb9a37b93f02913f9c39b3e
Greg <greg@gohan.(none)>
2010-05-14 22:24:13 -0700

    Forgot the file

--- lib/classifier/word_score.rb
+++ lib/classifier/word_score.rb
@@ -4,7 +4,28 @@
 
 module Classifier
 
-class WordCount
-end
+  # I really feel that WordScore shouldn't inherit from Bayes.
+  # But for now, this will do
+  class WordScore < Bayes
+
+    # Returns the score in each category for the given +text+
+    # TODO: cache +total+ after everyt training
+    def classifications(text)
+      score = Hash.new
+      @categories.each do |category, category_words|
+        score[category.to_s] = 0
+        total = category_words.values.inject(0) { |sum, element| sum + element }
+
+        text.word_hash.each do |word, count|
+          if freq = category_words[word]
+            score[category.to_s] +=  (freq / total.to_f) * count
+          end
+        end
+      end
+
+      score
+    end
+
+  end # class WordScore
 
 end
