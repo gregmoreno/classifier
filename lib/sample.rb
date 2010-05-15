c417748d8acedad788f27253ee66dcca817702a4
Greg <greg@gohan.(none)>
2010-05-14 23:48:18 -0700

    Fix loading of training data.

--- lib/sample.rb
+++ lib/sample.rb
@@ -2,11 +2,10 @@ require 'classifier'
 
 TRAINING_DATA_DIR = "../test/data"
 categories = {
-  'computers' => "#{TRAINING_DATA_DIR}/computers.txt",
-  'economy'   => "#{TRAINING_DATA_DIR}/economy.txt",
-  'health'    => "#{TRAINING_DATA_DIR}/health.txt"
+  :computers => "#{TRAINING_DATA_DIR}/computers.txt",
+  :economy   => "#{TRAINING_DATA_DIR}/economy.txt",
+  :health    => "#{TRAINING_DATA_DIR}/health.txt"
 }
-
 
 classifiers = {
   :bayes     => Classifier::Bayes.new(*categories.keys),
@@ -15,22 +14,26 @@ classifiers = {
 
 categories.each do |category, training_data|
   File.open(training_data) do |f|
+    text = f.read
     classifiers.each do |name, classifier|
-      classifier.train(category, f.read)
+      classifier.train(category, text)
     end
   end
 end
 
 samples = [ 
-  "we will someday find a medicine for cold",
-  "computing power doubles every 18 months",
+  "find a medicine for cold, good for government",
+  "medicine computing power for government doubles every 18 months",
   "the government raised interest rates once more"
 ]
 
 classifiers.each do |name, classifier|
   puts "Using #{name}"
   samples.each do |sample|
-    puts sample
-    puts "  #{classifier.classify(sample)}"
+    puts "  > #{sample}"
+    puts "  * #{classifier.classify(sample)}"
+    classifier.classifications(sample).each do |category, score|
+      printf "    %10s => %4.10f\n", category, score
+    end
   end
 end
